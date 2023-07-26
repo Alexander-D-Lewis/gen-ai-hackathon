0ce18bd408cdd2f6b9319ef1daadf72c3e188ab9
alewis <alewis@no10.gov.uk>
2023-07-26 17:47:47 +0100

    made into functions

--- import_data.py
+++ import_data.py
@@ -5,27 +5,38 @@ from glob import iglob
 from langchain.document_loaders import BSHTMLLoader
 from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
 
-if __name__ == "__main__":
 
+
+
+def load_docs():
     docs = []
     for file_path in iglob("downloaded_docs/*"):
         try:
             docs.extend(BSHTMLLoader(file_path=file_path).load())
             print(f"Loaded {file_path}")
         except Exception as e:
-            #  print(f"Error loading {file_path}: {e}")
             pass
+    return docs
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, overlap=100)
+
+def split_docs(docs):
+    text_splitter = CharacterTextSplitter()
 
     corpus_list = []
 
     for i, doc in enumerate(docs):
             splits = text_splitter.split_text(doc.page_content)
             corpus_list.append(splits)
+    return corpus_list
 
+
+
+if __name__ == "__main__":
+    docs = load_docs()
+    corpus_list = split_docs(docs)
+    
     store = FAISS.from_texts(corpus_list, OpenAIEmbeddings())
     store.save_local("faiss_index")
 
